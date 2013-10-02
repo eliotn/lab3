493d64f6620e7cc25dc51312857676da12882c5c
Eliot Glairon <ejg_dnd@yahoo.com>
2013-10-02 12:42:06 -0700

    1B done?

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,11 +3,4 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
-  # give the link_to
-  def sort_column_link(column, title=nil)
-    link_to title, {:sort => column}
-  end
-  def getThClass(column)
-    params[:sort] == column ? "hilite" : nil
-  end
 end
